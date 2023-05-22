48cbe4b81f3c2bbe642ca1adcd8218ce45d904b4
25akanksha25 <123538069+25akanksha25@users.noreply.github.com>
2023-05-23 01:26:45 +0530

    Create waste2.js

---
+++ waste2.js
@@ -0,0 +1,16 @@
+
+function showQueryPopup() {
+    var popup = document.getElementById('loginPopup');
+    popup.style.display = 'block';
+  }
+  
+  function closeQueryPopup() {
+    var popup = document.getElementById('loginPopup');
+    popup.style.display = 'none';
+  }
+  
+  function showsubmit() {
+    // Here, you can perform additional actions like creating the account or displaying a message
+    alert('Your Response Recorded Successfully!');
+  }
+  
