e485ccbda7bf251ffb4f0dcbcf28b4d262b83545
25akanksha25 <123538069+25akanksha25@users.noreply.github.com>
2023-05-23 01:25:55 +0530

    Create waste.js

---
+++ waste.js
@@ -0,0 +1,14 @@
+function showLoginPopup() {
+    var popup = document.getElementById('loginPopup2');
+    popup.style.display = 'block';
+  }
+  
+  function closeLoginPopup() {
+    var popup = document.getElementById('loginPopup2');
+    popup.style.display = 'none';
+  }
+  
+  function showCreateAccount() {
+    // Here, you can perform additional actions like creating the account or displaying a message
+    alert('Account Is Created Successfully');
+  }
